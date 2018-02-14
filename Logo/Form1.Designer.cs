81000404586cecb7cb6bbea673fb2004670f8ce8
Kiran Wotherspoon <kirawoth947@ed.amdsb.ca>
2018-02-14 09:51:52 -0800

    Add project files.

---
+++ Logo/Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace Logo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playButton = new System.Windows.Forms.Button();
+            this.countdownLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(110, 12);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(182, 41);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // countdownLabel
+            // 
+            this.countdownLabel.AutoSize = true;
+            this.countdownLabel.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.countdownLabel.ForeColor = System.Drawing.Color.White;
+            this.countdownLabel.Location = new System.Drawing.Point(73, 155);
+            this.countdownLabel.Name = "countdownLabel";
+            this.countdownLabel.Size = new System.Drawing.Size(265, 29);
+            this.countdownLabel.TabIndex = 1;
+            this.countdownLabel.Text = "Game will begin in: 3";
+            this.countdownLabel.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(413, 346);
+            this.Controls.Add(this.countdownLabel);
+            this.Controls.Add(this.playButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label countdownLabel;
+    }
+}
+
---
+++ Logo/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace Logo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //make sound effect play
+            SoundPlayer player = new SoundPlayer(Properties.Resources.startSound);
+            player.Play();
+            Thread.Sleep(500); //sync up sound and visual
+
+            //make button disappear
+            playButton.Visible = false;
+
+            //countdown starts
+            countdownLabel.Visible = true;
+            Refresh();
+            Thread.Sleep(1000);
+            countdownLabel.Text = "Game will start in: 2";
+            Refresh();
+            Thread.Sleep(1000);
+            countdownLabel.Text = "Game will start in: 1";
+            Refresh();
+            Thread.Sleep(1000);
+
+            //graphics set up
+            countdownLabel.Visible = false;
+            Refresh();
+            Graphics fg = this.CreateGraphics();
+            fg.Clear(Color.Green);
+
+            //draw elipise in red
+            SolidBrush drawBrush = new SolidBrush(Color.Red);
+            fg.FillEllipse(drawBrush, 100, 90, 225, 150);
+
+            //draw "Rams" in white in elipise
+            drawBrush.Color = Color.White;
+            Font ramsFont = new Font("Copperplate Gothic Bold", 22, FontStyle.Bold);
+            fg.DrawString("RAMS", ramsFont, drawBrush, 175, 150);
+
+            //draw C in front of Rams
+            Pen drawPen = new Pen(Color.White, 10);
+            fg.DrawArc(drawPen, 155, 130, 90, 70, 30, 300);
+
+            //draw rotated string to right of elipise
+            Font rotatedFont = new Font("DejaVu Serif", 8, FontStyle.Regular);
+            fg.TranslateTransform(325, 210);
+            fg.RotateTransform(270);
+            fg.DrawString("RAMS Arcade", rotatedFont, drawBrush, new Rectangle());
+        }
+    }
+}
